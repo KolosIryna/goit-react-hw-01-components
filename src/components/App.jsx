3ac91c2eacdf245fab54907cbea83fa30abc0a38
KolosIryna <127253667+KolosIryna@users.noreply.github.com>
2023-09-18 16:16:48 +0200

    friends

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,8 +4,11 @@ import user from '../data/user.json';
 import { Statistics } from './Statistics/Statistics';
 import data from '../data/data.json';
 
-// import { FriendList } from './FriendList/FriendList';
-// import friends from '../data/friends.json';
+import { FriendList } from './FriendList/FriendList';
+import friends from '../data/friends.json';
+
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import transactions from '../data/transactions.json';
 
 export const App = () => {
   return (
@@ -18,7 +21,8 @@ export const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={data} />
-      {/* <FriendList /> */}
+      <FriendList friends={friends} />;
+      <TransactionHistory items={transactions} />;
     </div>
   );
 };
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,19 @@
+import { FriendListItem } from '../FriendListItem/FriendListItem';
+import css from './FriendList.module.css';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.friend_list}>
+      {friends.map(friend => {
+        return (
+          <FriendListItem
+            key={friend.id}
+            avatar={friend.avatar}
+            name={friend.name}
+            isOnline={friend.isOnline}
+          />
+        );
+      })}
+    </ul>
+  );
+};
